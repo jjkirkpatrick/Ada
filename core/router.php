ddb95a8c7601df44e0d5e8825c5715d1d319b753
Oisian <Webcam1>
2018-03-07 15:43:12 +0000

    Added session class to handle session related activies including getting form data from a failed form. accessable from controllers and views
    added a dedicated authentication class to the core of the project

--- core/router.php
+++ core/router.php
@@ -54,9 +54,19 @@ class router
             //explode the url in to chunks
             $url = explode('/', $url);
             //assign the chunks to be the controller and the action in the controller
+            $count = (isset($url) ? count($url) : 0);
+            $vars = array();
+
+            if(isset($url[1])){
+                for ($i = 2; $i < $count ; $i++) {
+                    array_push($vars, $url[$i]);
+                }
+            }
+
             $this->uri = [
                 'controller' => (isset($url[0]) ? $url[0] : null),
                 'action' => (isset($url[1]) ? $url[1] : null),
+                'var' => (isset($vars[1]) ? $vars: null),
             ];
         }
     }
@@ -66,6 +76,8 @@ class router
     {
         $this->constructURIArray();
 
+        print_r($this->host . '/' . $this->uri['controller'] . (isset($this->uri['action']) ? '/' . $this->uri['action'] : null));
+
         //If the controller chunk of the URI is not set then go to the index page
         //equivalent of index/index
         if ($this->uri['controller'] == '') {
@@ -74,25 +86,25 @@ class router
             //create an instance of the controller and call the index function
             $controller = new index();
             $controller->index();
-        //if the controller chunk is set check the constructed URI with the valid built routes.
+            //if the controller chunk is set check the constructed URI with the valid built routes.
         } elseif (in_array($this->host . '/' . $this->uri['controller'] . (isset($this->uri['action']) ? '/' . $this->uri['action'] : null), $this->routes)) {
             //load the controller for the route
             require "controller/" . $this->uri['controller'] . '.php';
             $controller = new $this->uri['controller']();
             //if an action is set, test if it exists in the controller and call it, else call index
-            if(isset($this->uri['action'])){
-                if (method_exists($controller,$this->uri['action'])) {
-                    $controller->{$this->uri['action']}();
+            if (isset($this->uri['action'])) {
+                if (method_exists($controller, $this->uri['action'])) {
+                    $controller->{$this->uri['action']}($this->uri['var']);
                 } else {
                     $controller->{"index"}();
                 }
-             //if controller is set but no method go the index of the controller.
-            }else{
+                //if controller is set but no method go the index of the controller.
+            } else {
                 $controller->{"index"}();
             }
-        //if the route is not valid end the application
+            //if the route is not valid end the application
         } else {
-            die("invalid route");
+            die(" invalid route");
         }
 
 
