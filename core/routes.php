01525b559a17697a9b0e05d1009a3285e9cb0f0d
Oisian <Webcam1>
2018-03-07 16:08:17 +0000

    Added session class to handle session related activies including getting form data from a failed form. accessable from controllers and views
    added a dedicated authentication class to the core of the project

--- core/routes.php
+++ core/routes.php
@@ -4,6 +4,18 @@
 # {h} is inplace of domain
 # {w} is a wildcard
 
+
+//todo impliment wild cards, so only routes with wildcard defined can make use of them
+//wildcard example  /account/user/profile
+//                  /account/{w}/profile
+//                  /account/5/profile
+
+
+//todo think about explicitly defining where route goes
+//$routes = array(
+//          '{h}/' => "index/index,
+//          '{h}/account/{w}/profile' => "account/profile/{w}",
+//where profile is the controller method and {w} is the variable place holder
 $routes = array(
     '{h}/',
     '{h}/account',
@@ -11,4 +23,5 @@ $routes = array(
     '{h}/account/register',
     '{h}/account/logout',
     '{h}/index',
+    '{h}/test/test',
 );
